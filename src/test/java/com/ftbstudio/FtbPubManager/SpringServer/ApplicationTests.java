3f57ed8deed8727903ce065f65f2f6ce2391a12e
francescopanico <frpanico96@gmail.com>
2024-01-03 16:54:27 +0100

    [FTB] PubManagerSpring - first commit

---
+++ src/test/java/com/ftbstudio/FtbPubManager/SpringServer/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftbstudio.FtbPubManager.SpringServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
